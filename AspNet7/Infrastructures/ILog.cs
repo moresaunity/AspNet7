c73d5f8ccff69b6615b5e37097775350770c6744
Moresa <Moresa@DESKTOP-524A0QJ>
2023-05-30 04:30:41 +0330

    Add project files.

---
+++ AspNet7/Infrastructures/ILog.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNet7.Infrastructures
+{
+    public interface ILog
+    {
+        void Execute(string message);
+    }
+    public class ConsoleLog : ILog
+    {
+        public void Execute(string message) 
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+}
